1600467de61e973884ca6c5d8f1312e30397f5bd
nuck118 <125555351+nuck118@users.noreply.github.com>
2025-03-03 23:52:33 +0100

    m

--- js/navbar.js
+++ js/navbar.js
@@ -134,7 +134,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function handleAdminLogin(username, password) {
-        if (username === 'admin' && password === 'password') {
+        if (username === 'admin' && password === 'securepassword') {
             window.location.href = "admin/dashboard.html";
         } else {
             alert('Invalid credentials');
