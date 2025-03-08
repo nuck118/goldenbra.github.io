85323c6073bb0349086d15f7e2a5c83c213c27c2
nuck118 <125555351+nuck118@users.noreply.github.com>
2025-03-08 01:52:39 +0100

    d

--- js/navbar.js
+++ js/navbar.js
@@ -4,12 +4,12 @@ document.addEventListener('DOMContentLoaded', function() {
     header.className = 'bg-gray-900 text-white py-4 shadow-md';
     header.innerHTML = `
         <div class="container mx-auto flex justify-between items-center px-6">
-            <h1 class="text-2xl font-bold">GOLDENBRA</h1>
+            <h1 href="index.html" class="text-2xl font-bold">GOLDENBRA</h1>
             <nav class="hidden md:flex">
                 <ul class="flex space-x-6">
                     ${createNavLinks()}
                     <li class="relative">
-                        <a href="login.html" class="flex items-center space-x-2 hover:text-blue-400">
+                        <a href="admin/login.html" class="flex items-center space-x-2 hover:text-blue-400">
                             <i class="fas fa-lock"></i>
                             <span>Admin</span>
                         </a>
